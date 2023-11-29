698eda191ec0f859878f936a7695bb49b6bd6be1
Grzegorz <g.w.mika@gmail.com>
2023-11-29 09:18:11 +0100

    mvc

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,13 +1,26 @@
-const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
 // https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+const controlRecipe = async function() {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    recipeView.renderSpinner();
+    await model.loadRecipe(id);
+    recipeView.render(model.state.recipe);
+
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+const init = function() {
+  recipeView.addHandlerRender(controlRecipe)
+}
+init()
